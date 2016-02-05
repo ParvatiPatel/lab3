d4244e5be6611d08c3861cba1e0f393b2b5982e8
ParvatiPatel <paro.patel@rocketmail.com>
2016-02-04 16:34:56 -0800

    let

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,24 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* GET services page. */
+router.get('/service', function(req, res, next) {
+  res.render('service', { title: 'Service' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About US' });
+});
+
+/* GET social page. */
+router.get('/social', function(req, res, next) {
+  res.render('social', { title: 'Social' });
+});
+
+/* GET contact page. */
+router.get('/ContactUs', function(req, res, next) {
+  res.render('ContactUs', { title: 'Contact Us' });
+});
 
 module.exports = router;
